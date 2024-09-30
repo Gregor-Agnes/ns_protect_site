8e5bd061328de02a096654acdeea5e5bc6c94bf7
Gregor <ga@zwo3.de>
2024-09-30 17:00:46 +0200

    refactor: update plugin configuration for TYPO3 v10+
    - Replace TYPO3_MODE check with TYPO3 for compatibility.
    - Remove conditional class assignment and streamline plugin configurations.
    - Separate plugin configurations for 'load', 'login', and 'form' actions.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,19 +1,30 @@
 <?php
-defined('TYPO3_MODE') || die('Access denied.');
+defined('TYPO3') || die('Access denied.');
 
-if (version_compare(TYPO3_branch, '10.0', '>=')) {
-    $moduleClass = \Nitsan\NsProtectSite\Controller\ProtectPagesController::class;
-} else {
-    $moduleClass = 'ProtectPages';
-}
+    //$moduleClass = \Nitsan\NsProtectSite\Controller\ProtectPagesController::class;
+
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Nitsan.NsProtectSite',
-    'Nsprotectsite',
+    'NsProtectSite',
+    'NsprotectsiteLoad',
     [
-        $moduleClass => 'form,login,load'
+        \Nitsan\NsProtectSite\Controller\ProtectPagesController::class => 'load'
+    ]
+);
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'NsProtectSite',
+    'NsprotectsiteLogin',
+    [
+        \Nitsan\NsProtectSite\Controller\ProtectPagesController::class => 'login'
     ],
     // non-cacheable actions
     [
-        $moduleClass => 'login'
+        \Nitsan\NsProtectSite\Controller\ProtectPagesController::class => 'login'
+    ]
+);
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'NsProtectSite',
+    'NsprotectsiteForm',
+    [
+        \Nitsan\NsProtectSite\Controller\ProtectPagesController::class => 'form'
     ]
 );
