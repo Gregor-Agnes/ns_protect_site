ad551f614e09ccddf8c9da5070c4f41249a4508f
Gregor <ga@zwo3.de>
2024-09-30 17:00:18 +0200

    refactor: streamline TCA configurations for pages
    - Simplified the check for TYPO3 mode definition.
    - Merged field definitions directly into addTCAcolumns.
    - Cleaned up conditional handling for different TYPO3 branches.

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,50 +1,34 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
-
-// Configure new fields:
-$fields = [
-  'tx_nsprotectsite_protection' => [
-    'label' => 'LLL:EXT:ns_protect_site/Resources/Private/Language/locallang_db.xlf:enableprotect',
-    'exclude' => 1,
-    'config' => [
-      'type' => 'check',
-      'default' => 0,
-    ],
-    'onChange' => 'reload'
-  ],
-  'tx_nsprotectsite_protect_password' => [
-    'exclude' => 1,
-    'displayCond' => 'FIELD:tx_nsprotectsite_protection:>:0',
-    'label' => 'LLL:EXT:ns_protect_site/Resources/Private/Language/locallang_db.xlf:addpass',
-    'config' => [
-        'type' => 'input',
-        'max' => 100,
-        'eval' => 'trim,required,password,saltedPassword',
-    ]
-  ]
-];
+defined('TYPO3') or die();
 
 // Add new fields to pages:
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $fields);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages',
+    [
+        'tx_nsprotectsite_protection' => [
+            'label' => 'LLL:EXT:ns_protect_site/Resources/Private/Language/locallang_db.xlf:enableprotect',
+            'exclude' => 1,
+            'config' => [
+                'type' => 'check',
+                'default' => 0,
+            ],
+            'onChange' => 'reload'
+        ],
+        'tx_nsprotectsite_protect_password' => [
+            'exclude' => 1,
+            'displayCond' => 'FIELD:tx_nsprotectsite_protection:>:0',
+            'label' => 'LLL:EXT:ns_protect_site/Resources/Private/Language/locallang_db.xlf:addpass',
+            'config' => [
+                'type' => 'input',
+                'max' => 100,
+                'eval' => 'trim,required,password,saltedPassword',
+            ]
+        ]
+    ]);
 
-// Add the new palette:
-$GLOBALS['TCA']['pages']['palettes']['tx_nsprotectsite'] = [
-    'showitem' => 'tx_nsprotectsite_protection, --linebreak--, tx_nsprotectsite_protect_password',
-];
 
-// Make fields visible in the TCEforms:
-if (version_compare(TYPO3_branch, '10.0', '<')) {
+
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
         'pages', // Table name
-    '--div--;LLL:EXT:ns_protect_site/Resources/Private/Language/locallang_db.xlf:tx_nsprotectsite_domain_model_protectpages;tx_nsprotectsite',
+    '--div--;Seite schützen, tx_nsprotectsite_protection, --linebreak--, tx_nsprotectsite_protect_password',
         '1' // List of specific types to add the field list to. (If empty, all type entries are affected)
     );
-} else {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
-        'pages', // Table name
-    '--div--;LLL:EXT:ns_protect_site/Resources/Private/Language/locallang_db.xlf:tx_nsprotectsite_domain_model_protectpages,--palette--;LLL:EXT:ns_protect_site/Resources/Private/Language/locallang_db.xlf:tx_nsprotectsite_domain_model_protectpages;tx_nsprotectsite',
-        '1' // List of specific types to add the field list to. (If empty, all type entries are affected)
-    );
-}
